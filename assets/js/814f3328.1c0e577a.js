87acd4369b8e08c00e426c16e1b19c310f283499
Friedrich Wicke <84902954+fridowicke@users.noreply.github.com>
2023-03-02 00:28:22 +0100

    Deploy website - based on 1f6a5ab86ad66a633ba65a7b263d9023dfde9a01

---
+++ assets/js/814f3328.1c0e577a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksophiaedulabs_com=self.webpackChunksophiaedulabs_com||[]).push([[535],{5641:s=>{s.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hi from Sophia Edu Labs","permalink":"/blog/first-blog-post"}]}')}}]);
---
+++ assets/js/c7fc3942.69a7359e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksophiaedulabs_com=self.webpackChunksophiaedulabs_com||[]).push([[113],{9449:s=>{s.exports=JSON.parse('{"permalink":"/blog/tags/sophia","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
