11ee0986d10118a087e141366949485d5308c4e5
Friedrich Wicke <84902954+fridowicke@users.noreply.github.com>
2023-03-01 11:58:57 +0100

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://sophiaedulabs.com',
+  url: 'http://sophia-edu-labs.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Sophia Edu Labs', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Sophia-Edu-Labs', // Usually your GitHub org/user name.
+  projectName: 'sophiaedulabs.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: "main",
+
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
